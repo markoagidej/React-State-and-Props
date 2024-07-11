b76f7e0ed28ad4a7ecf1ebd9adf485eb2af4cd1a
Marko <markogidej@yahoo.com>
2024-07-11 17:46:54 -0400

    Genre toggle done (Q2T4)

--- src/components/2_Enhancing_Functional_Components.jsx
+++ src/components/2_Enhancing_Functional_Components.jsx
@@ -5,10 +5,10 @@ import React, { useState } from 'react';
 const MoviesList = () => {
     // Initialize state with a list of movies
     const [movieList, setMovies] = useState([
-        {id: 1, title: 'Sacrifice', description: 'World War III has erupted, and the end of the world is near. In order to avert the apocalypse, Alexander makes a bargain with God', genre: 'drama', descVisible: false},
-        {id: 2, title: 'Nostalgia', description: 'To investigate the life of Russian composer Pavel Sosnovsky, Andrei Gorchakov goes to Italy, where Sosnovsky resided before his suicide.', genre: 'drama', descVisible: false},
-        {id: 3, title: 'Voyage in Time', description: 'Documents the travels in Italy of the director Andrei Tarkovsky with the script writer Tonino Guerra in preparation for the making of his film Nostalghia', genre: 'documentary', descVisible: false},
-        {id: 4, title: 'Stalker', description: 'In an unnamed country at an unspecified time, there is a fiercely protected post-apocalyptic wasteland known as The Zone.', genre: 'science fiction', descVisible: false}
+        {id: 1, title: 'Sacrifice', description: 'World War III has erupted, and the end of the world is near. In order to avert the apocalypse, Alexander makes a bargain with God', genre: 'drama', descVisible: false, genreHide: false},
+        {id: 2, title: 'Nostalgia', description: 'To investigate the life of Russian composer Pavel Sosnovsky, Andrei Gorchakov goes to Italy, where Sosnovsky resided before his suicide.', genre: 'drama', descVisible: false, genreHide: false},
+        {id: 3, title: 'Voyage in Time', description: 'Documents the travels in Italy of the director Andrei Tarkovsky with the script writer Tonino Guerra in preparation for the making of his film Nostalghia', genre: 'documentary', descVisible: false, genreHide: false},
+        {id: 4, title: 'Stalker', description: 'In an unnamed country at an unspecified time, there is a fiercely protected post-apocalyptic wasteland known as The Zone.', genre: 'science fiction', descVisible: false, genreHide: false}
     ]);
     
     const removeMovie = (event) => {
@@ -37,32 +37,80 @@ const MoviesList = () => {
         setMovies(movieList)
     } 
 
-    return (
-        <div>
-            <ul>
-                {movieList.map((movie, index) => {
-                    return (
-                        <div>
-                            {movie.descVisible ? (
+    const [dramaOnly, setDramaOnly] = useState(false)
+    const toggleDramas = () => {
+        let newState = dramaOnly ? false : true
+        setDramaOnly(newState)
+        // movieList.map((movie) => {
+        //     console.log(movie)
+        //     (movie.genre != "drama") ? (movie.genreHide = dramaOnly) : (movie.genreHide = !dramaOnly)
+        // })
+        // console.log(movieList)
+        // setMovies(movieList)
+    }
+// 
+    if (dramaOnly) {
+        return (
+            <div>
+                <button onClick={toggleDramas}>Toggle Dramas</button>
+                <ul>
+                    {movieList.map((movie, index) => {
+                        if (movie.genre == "drama") {
+                            return (
                                 <div>
-                                    <li onClick={hideDesc} data-value={index} key={movie.id}>{movie.title}</li>
-                                    <p>{movie.description}</p>
+                                    {movie.descVisible ? (
+                                        <div>
+                                            <li onClick={hideDesc} data-value={index} key={movie.id}>{movie.title}</li>
+                                            <p>{movie.description}</p>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <li onClick={showDesc} data-value={index} key={movie.id}>{movie.title}</li>
+                                        </div>
+                                    )}
                                 </div>
-                            ) : (
-                                <div>
-                                    <li onClick={showDesc} data-value={index} key={movie.id}>{movie.title}</li>
-                                </div>
-                            )}
-                        </div>
-                    )
-                })}
-                {movieList.map((movie) => {
-                    return (
-                        <button data-value={movie.id} onClick={removeMovie}>Remove {movie.title}</button>
-                    )
-                })}
-            </ul>
-        </div>
-    );
+                            )
+                        } else {
+                            return (<div></div>)
+                        }
+                    })}
+                    {movieList.map((movie) => {
+                        return (
+                            <button data-value={movie.id} onClick={removeMovie}>Remove {movie.title}</button>
+                        )
+                    })}
+                </ul>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <button onClick={toggleDramas}>Toggle Dramas</button>
+                <ul>
+                    {movieList.map((movie, index) => {                                 
+                        return (
+                            <div>
+                                {movie.descVisible ? (
+                                    <div>
+                                        <li onClick={hideDesc} data-value={index} key={movie.id}>{movie.title}</li>
+                                        <p>{movie.description}</p>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <li onClick={showDesc} data-value={index} key={movie.id}>{movie.title}</li>
+                                    </div>
+                                )}
+                            </div>
+                        )
+                    })}
+                    {movieList.map((movie) => {
+                        return (
+                            <button data-value={movie.id} onClick={removeMovie}>Remove {movie.title}</button>
+                        )
+                    })}
+                </ul>
+            </div>
+        );
+    }
 };
 export default MoviesList;
