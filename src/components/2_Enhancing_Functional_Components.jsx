4c69366a3197cef1355787c0e6801de4b3c5b097
Marko <markogidej@yahoo.com>
2024-07-11 14:49:51 -0400

    Remove and description toggle work seperately, but not together. experimenting with locations of code (commented out)

--- src/components/2_Enhancing_Functional_Components.jsx
+++ src/components/2_Enhancing_Functional_Components.jsx
@@ -2,17 +2,37 @@
 
 import React, { useState } from 'react';
 
+// const [movieList, setMovies] = useState([
+//     {id: 1, title: 'Sacrifice', description: 'World War III has erupted, and the end of the world is near. In order to avert the apocalypse, Alexander makes a bargain with God', genre: 'drama', hidden: true},
+//     {id: 2, title: 'Nostalgia', description: 'To investigate the life of Russian composer Pavel Sosnovsky, Andrei Gorchakov goes to Italy, where Sosnovsky resided before his suicide.', genre: 'drama', hidden: true},
+//     {id: 3, title: 'Voyage in Time', description: 'Documents the travels in Italy of the director Andrei Tarkovsky with the script writer Tonino Guerra in preparation for the making of his film Nostalghia', genre: 'documentary', hidden: true},
+//     {id: 4, title: 'Stalker', description: 'In an unnamed country at an unspecified time, there is a fiercely protected post-apocalyptic wasteland known as The Zone.', genre: 'science fiction', hidden: true}
+// ]);
+
+// const Movies = ({movieList}) => {
+//     return (
+//         <div>
+//             <ul>
+//             {movieList.map((movie, index) => {
+//                 <li key={index}>{movie.title}</li>
+//             })}
+//             </ul>
+//         </div>
+//     )
+// };
+
 const MoviesList = () => {
     // Initialize state with a list of movies
-    const [movieList, setMovies] = useState([
-        {id: 1, title: 'Sacrifice', description: 'World War III has erupted, and the end of the world is near. In order to avert the apocalypse, Alexander makes a bargain with God', genre: 'drama'},
-        {id: 2, title: 'Nostalgia', description: 'To investigate the life of Russian composer Pavel Sosnovsky, Andrei Gorchakov goes to Italy, where Sosnovsky resided before his suicide.', genre: 'drama'},
-        {id: 3, title: 'Voyage in Time', description: 'Documents the travels in Italy of the director Andrei Tarkovsky with the script writer Tonino Guerra in preparation for the making of his film Nostalghia', genre: 'documentary'},
-        {id: 4, title: 'Stalker', description: 'In an unnamed country at an unspecified time, there is a fiercely protected post-apocalyptic wasteland known as The Zone.', genre: 'science fiction'}
-    ]);
 
+    const [movieList, setMovies] = useState([
+        {id: 1, title: 'Sacrifice', description: 'World War III has erupted, and the end of the world is near. In order to avert the apocalypse, Alexander makes a bargain with God', genre: 'drama', hidden: true},
+        {id: 2, title: 'Nostalgia', description: 'To investigate the life of Russian composer Pavel Sosnovsky, Andrei Gorchakov goes to Italy, where Sosnovsky resided before his suicide.', genre: 'drama', hidden: true},
+        {id: 3, title: 'Voyage in Time', description: 'Documents the travels in Italy of the director Andrei Tarkovsky with the script writer Tonino Guerra in preparation for the making of his film Nostalghia', genre: 'documentary', hidden: true},
+        {id: 4, title: 'Stalker', description: 'In an unnamed country at an unspecified time, there is a fiercely protected post-apocalyptic wasteland known as The Zone.', genre: 'science fiction', hidden: true}
+    ]);
     
     const removeMovie = (event) => {
+        console.log("Remove Start")
         const id = event.currentTarget.getAttribute("data-value");
         let moviesCopy = movieList;
         let movieIndex = 0
@@ -22,35 +42,67 @@ const MoviesList = () => {
             }
         });
         moviesCopy.splice(movieIndex, 1);
-        console.log(moviesCopy)
         setMovies(moviesCopy);
-    }
+        console.log("Remove End")
+    }    
 
-    const [descVisible, setVisible] = useState(false);
-                
-    const showDesc = () => {
-        setVisible(true);
-    }
+    // const toggleDescription = (event) => {
 
-    const hideDesc = () => {
-        setVisible(false);
-    } 
+    //     const [descVisible, setVisible] = useState(false);
+                    
+    //     const showDesc = () => {
+    //         setVisible(true);
+    //     }
+    
+    //     const hideDesc = () => {
+    //         setVisible(false);
+    //     } 
+
+    //     const id = event.currentTarget.getAttribute("data-value");
+    //     movieList.forEach((listMovie) => {
+    //         if (listMovie.id == id) {
+    //             if (listMovie.hidden) {
+    //                 listMovie.hidden = false
+    //             } else {
+    //                 listMovie.hidden = true
+    //             }
+    //         }
+    //     });
+    // }
 
     return (
         <div>
             <ul>
                 {movieList.map((movie) => {
-                    return (
+                    console.log(movie.id)
+                    console.log("Movie Before State")
+                    const [descVisible, setVisible] = useState(false);
+                    console.log("Movie After State")
+                                
+                    const showDesc = () => {
+                        console.log("Movie before show")
+                        setVisible(true);
+                        console.log("Movie after show")
+                    }
+                
+                    const hideDesc = () => {
+                        console.log("Movie before hide")
+                        setVisible(false);
+                        console.log("Movie after hide")
+                    } 
+                        
+                    return (                        
                         <div>
                             {descVisible ? (
                                 <div>
-                                    <li onClick={hideDesc} key={movie.id}>{movie.title}<button data-value={movie.id} onClick={removeMovie}>Remove</button></li>
+                                    <li onClick={hideDesc} key={movie.id}>{movie.title}</li>
                                     <p>{movie.description}</p>
-                                    <p>{movie.id}</p>
+                                    <button data-value={movie.id} onClick={removeMovie}>Remove</button>
                                 </div>
                             ) : (
                                 <div>
-                                    <li onClick={showDesc} key={movie.id}>{movie.title}<button data-value={movie.id} onClick={removeMovie}>Remove</button></li>
+                                    <li onClick={showDesc} key={movie.id}>{movie.title}</li>
+                                    <button data-value={movie.id} onClick={removeMovie}>Remove</button>
                                 </div>
                             )}
                         </div>
